5a7c695627c3af1b8d2da6b8861ea7482004de86
Meduza3 <jellyfish200325@gmail.com>
2023-12-22 23:20:24 +0100

    Working demo!

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-var message = 'Hello World';
-console.log(message);
 var Axis = /** @class */ (function () {
     function Axis(name) {
         this.name = name;
@@ -10,7 +8,7 @@ var Axis = /** @class */ (function () {
 }());
 var Party = /** @class */ (function () {
     function Party(name, count, values) {
-        this.vote = Vote.Hold;
+        this.vote = Vote.HOLD;
         this.count_per_opinion = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,13 +20,13 @@ var Party = /** @class */ (function () {
         this.values = values;
         for (var i = 0; i < 4; i++) {
             for (var j = 0; j < 10; j++) {
-                if (this.values[i] == j - 2 || this.values[i] == j + 2) {
+                if (this.values[i] == j - 1 || this.values[i] == j + 3) {
                     this.count_per_opinion[i][j] = this.getExtremeCount();
                 }
-                else if (this.values[i] == j - 1 || this.values[i] == j + 1) {
+                else if (this.values[i] == j || this.values[i] == j + 2) {
                     this.count_per_opinion[i][j] = this.getLeaningCount();
                 }
-                else if (this.values[i] == j) {
+                else if (this.values[i] == j + 1) {
                     this.count_per_opinion[i][j] = this.getBasisCount();
                 }
                 else {
@@ -37,6 +35,76 @@ var Party = /** @class */ (function () {
             }
         }
     }
+    Party.prototype.voteAtRandom = function () {
+        var p = Math.random();
+        if (p <= 1 / 3) {
+            this.vote = Vote.FOR;
+        }
+        else if (p <= 2 / 3) {
+            this.vote = Vote.AGAINST;
+        }
+        else {
+            this.vote = Vote.HOLD;
+        }
+    };
+    Party.prototype.findClosestParty = function (parties, axis, legislation, vote) {
+        var closestParty = null;
+        var minDifference = Number.MAX_VALUE;
+        var party_opinion = this.values[axis.order];
+        if (vote == Vote.HOLD) {
+            return this;
+        }
+        for (var party in parties) {
+        }
+        if (vote == Vote.FOR) {
+            if (legislation.values[axis.order] <= 5) {
+                for (var _i = 0, parties_1 = parties; _i < parties_1.length; _i++) {
+                    var party = parties_1[_i];
+                    var difference = party_opinion - party.values[axis.order];
+                    if (difference < minDifference && difference <= 0 && party.vote != this.vote) {
+                        minDifference = difference;
+                        closestParty = party;
+                    }
+                }
+            }
+            else {
+                for (var _a = 0, parties_2 = parties; _a < parties_2.length; _a++) {
+                    var party = parties_2[_a];
+                    var difference = party_opinion - party.values[axis.order];
+                    if (difference < minDifference && difference >= 0 && party.vote != this.vote) {
+                        minDifference = difference;
+                        closestParty = party;
+                    }
+                }
+            }
+        }
+        else if (vote == Vote.AGAINST) {
+            if (legislation.values[axis.order] <= 5) {
+                for (var _b = 0, parties_3 = parties; _b < parties_3.length; _b++) {
+                    var party = parties_3[_b];
+                    var difference = party_opinion - party.values[axis.order];
+                    if (difference < minDifference && difference >= 0 && party.vote != this.vote) {
+                        minDifference = difference;
+                        closestParty = party;
+                    }
+                }
+            }
+            else {
+                for (var _c = 0, parties_4 = parties; _c < parties_4.length; _c++) {
+                    var party = parties_4[_c];
+                    var difference = party_opinion - party.values[axis.order];
+                    if (difference < minDifference && difference <= 0 && party.vote != this.vote) {
+                        minDifference = difference;
+                        closestParty = party;
+                    }
+                }
+            }
+        }
+        if (closestParty == null) {
+            return new Party("Niezrzeszeni", 0, [5, 6, 5, 6]);
+        }
+        return closestParty;
+    };
     Party.prototype.getExtremeCount = function () {
         return this.count * 0.05;
     };
@@ -49,9 +117,38 @@ var Party = /** @class */ (function () {
     Party.prototype.setVote = function (vote) {
         this.vote = vote;
     };
-    Party.prototype.calculateMadPerAxis = function (legislation, axis) {
-        var party_opinion = this.values[axis.order];
-        var legislation_value = legislation.values[axis.order];
+    Party.prototype.calculateMad = function (legislation, axis) {
+        var mad = 0;
+        if (this.vote == Vote.HOLD) {
+            return mad;
+        }
+        if (legislation.values[axis.order] <= 5) {
+            var tolerance_barrier = legislation.values[axis.order] + 1;
+            if (this.vote == Vote.FOR) { //LEWICOWA ZA
+                for (var i = 9; i > tolerance_barrier - 1; i--) {
+                    mad += this.count_per_opinion[axis.order][i];
+                }
+            }
+            else { // LEWICOWA PRZECIW ???
+                for (var i = 0; i <= tolerance_barrier; i++) {
+                    mad += this.count_per_opinion[axis.order][i];
+                }
+            }
+        }
+        else {
+            var tolerance_barrier = legislation.values[axis.order] - 1;
+            if (this.vote == Vote.FOR) { //PRAWICOWA ZA
+                for (var i = 0; i < tolerance_barrier; i++) {
+                    mad += this.count_per_opinion[axis.order][i];
+                }
+            }
+            else { //PRAWICOWA PRZECIW
+                for (var i = 9; i >= tolerance_barrier - 1; i--) {
+                    mad += this.count_per_opinion[axis.order][i];
+                }
+            }
+        }
+        return Math.ceil(mad);
     };
     return Party;
 }());
@@ -65,26 +162,93 @@ var Legislation = /** @class */ (function () {
 }());
 var Vote;
 (function (Vote) {
-    Vote[Vote["For"] = 0] = "For";
-    Vote[Vote["Against"] = 1] = "Against";
-    Vote[Vote["Hold"] = 2] = "Hold";
+    Vote["FOR"] = "FOR";
+    Vote["AGAINST"] = "AGAINST";
+    Vote["HOLD"] = "HOLD";
 })(Vote || (Vote = {}));
 var numberOfAxes = 0;
-function calculateChanges(parties, legislation) {
-    for (var _i = 0, parties_1 = parties; _i < parties_1.length; _i++) {
-        var party = parties_1[_i];
-        // party.calculateAngryCount();
+function calculateChanges(parties, axes, legislation) {
+    for (var _i = 0, parties_5 = parties; _i < parties_5.length; _i++) {
+        var party = parties_5[_i];
+        if (party.vote != Vote.HOLD) {
+            for (var _a = 0, axes_1 = axes; _a < axes_1.length; _a++) {
+                var axis = axes_1[_a];
+                var mad = party.calculateMad(legislation, axis);
+                var furious = Math.ceil(mad / 5);
+                console.log(party.name + " " + axis.name + "Mad: " + mad);
+                var closestParty = party.findClosestParty(parties, axis, legislation, party.vote);
+                console.log(party.name + " " + axis.name + "ClosestParty: " + closestParty.name);
+                party.count -= furious;
+                console.log(party.name + " " + axis.name + "Party.count - mad: " + party.count);
+                if (closestParty == null) {
+                    niezrzeszeni += furious;
+                }
+                else {
+                    closestParty.count += furious;
+                    console.log(party.name + " " + axis.name + "ClosestParty.count + mad: " + closestParty.count);
+                }
+            }
+        }
+    }
+    recalculateCountPerOpinion(parties);
+}
+function recalculateCountPerOpinion(parties) {
+    for (var _i = 0, parties_6 = parties; _i < parties_6.length; _i++) {
+        var party = parties_6[_i];
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 10; j++) {
+                if (party.values[i] == j - 1 || party.values[i] == j + 3) {
+                    party.count_per_opinion[i][j] = party.getExtremeCount();
+                }
+                else if (party.values[i] == j || party.values[i] == j + 2) {
+                    party.count_per_opinion[i][j] = party.getLeaningCount();
+                }
+                else if (party.values[i] == j + 1) {
+                    party.count_per_opinion[i][j] = party.getBasisCount();
+                }
+                else {
+                    party.count_per_opinion[i][j] = 0;
+                }
+            }
+        }
     }
 }
+function getFor(parties) {
+    var count = 0;
+    for (var _i = 0, parties_7 = parties; _i < parties_7.length; _i++) {
+        var party = parties_7[_i];
+        if (party.vote == Vote.FOR)
+            count += party.count;
+    }
+    return count;
+}
+function getAgainst(parties) {
+    var count = 0;
+    for (var _i = 0, parties_8 = parties; _i < parties_8.length; _i++) {
+        var party = parties_8[_i];
+        if (party.vote == Vote.AGAINST)
+            count += party.count;
+    }
+    return count;
+}
+function getHold(parties) {
+    var count = 0;
+    for (var _i = 0, parties_9 = parties; _i < parties_9.length; _i++) {
+        var party = parties_9[_i];
+        if (party.vote == Vote.HOLD)
+            count += party.count;
+    }
+    return count;
+}
 main();
+var niezrzeszeni = 0;
 function main() {
     var parties = [];
-    var niezrzeszeni = 0;
     var pis = new Party("Prawo i Sprawiedliwość", 115, [4, 3, 7, 7]);
     parties.push(pis);
     var ko = new Party("Koalicja Obywatelska", 115, [6, 5, 4, 4]);
     parties.push(ko);
-    var lewica = new Party("Nowa Lewica", 115, [3, 3, 3, 3]);
+    var lewica = new Party("Nowa Lewica", 115, [8, 3, 3, 3]);
     parties.push(lewica);
     var konfa = new Party("Konfederacja", 115, [8, 7, 8, 8]);
     parties.push(konfa);
@@ -97,11 +261,14 @@ function main() {
     axes.push(culture);
     var eu = new Axis("Stosunek do UE");
     axes.push(eu);
-    console.log(parties, axes);
-    var invitro = new Legislation("Finansowanie in Vitro", fiscal_policy, [-1, 4, 4, -1]);
-    pis.setVote(Vote.Against);
-    konfa.setVote(Vote.Hold);
-    lewica.setVote(Vote.For);
-    ko.setVote(Vote.For);
-    calculateChanges(parties, invitro);
+    var legislation = new Legislation("Ustawa", economic_policy, [7]);
+    pis.voteAtRandom();
+    ko.voteAtRandom();
+    lewica.voteAtRandom();
+    ko.voteAtRandom();
+    console.log(parties);
+    console.log("Niezrzeszeni: " + niezrzeszeni);
+    calculateChanges(parties, axes, legislation);
+    console.log(parties);
+    console.log("Niezrzeszeni: " + niezrzeszeni);
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-let message: string = 'Hello World'!;
-
-console.log(message);
-
 class Axis {
     name: string;
     order: number;
@@ -17,7 +13,7 @@ class Party {
     name: string;
     count: number;
     values: number[];
-    vote: Vote = Vote.Hold;
+    vote: Vote = Vote.HOLD;
     count_per_opinion: number[][] = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
@@ -32,11 +28,11 @@ class Party {
 
         for(let i = 0; i < 4; i++){
             for(let j = 0; j < 10; j++){
-                if(this.values[i] == j - 2 || this.values[i] == j + 2){
+                if(this.values[i] == j - 1 || this.values[i] == j + 3){
                     this.count_per_opinion[i][j] = this.getExtremeCount();
-                } else if(this.values[i] == j - 1 || this.values[i] == j + 1){
+                } else if(this.values[i] == j || this.values[i] == j + 2){
                     this.count_per_opinion[i][j] = this.getLeaningCount();
-                } else if(this.values[i] == j){
+                } else if(this.values[i] == j + 1){
                     this.count_per_opinion[i][j] = this.getBasisCount();
                 } else {
                     this.count_per_opinion[i][j] = 0;
@@ -45,6 +41,72 @@ class Party {
         }
     }
 
+    voteAtRandom(){
+        let p = Math.random()
+        if(p <= 1/3){
+            this.vote = Vote.FOR;
+        } else if(p <= 2/3) {
+            this.vote = Vote.AGAINST;
+        } else {
+            this.vote = Vote.HOLD;
+        }
+    }
+
+    findClosestParty(parties: Party[], axis: Axis, legislation: Legislation, vote: Vote): Party{
+        let closestParty: Party | null = null;
+        let minDifference: number = Number.MAX_VALUE;
+        let party_opinion = this.values[axis.order];
+
+        if(vote == Vote.HOLD){
+            return this;
+        }
+
+        for(let party in parties){
+
+        }
+
+        if(vote == Vote.FOR){
+            if(legislation.values[axis.order] <= 5){
+                for(let party of parties){
+                    const difference = party_opinion - party.values[axis.order];
+                    if(difference < minDifference && difference <= 0 && party.vote != this.vote){
+                        minDifference = difference;
+                        closestParty = party;
+                    }
+                }
+            } else {
+                for(let party of parties){
+                    const difference = party_opinion - party.values[axis.order];
+                    if(difference < minDifference && difference >= 0 && party.vote != this.vote){
+                        minDifference = difference;
+                        closestParty = party;
+                    }
+                }
+            }
+        } else if(vote == Vote.AGAINST){
+            if(legislation.values[axis.order] <= 5){
+                for(let party of parties){
+                    const difference = party_opinion - party.values[axis.order];
+                    if(difference < minDifference && difference >= 0 && party.vote != this.vote){
+                        minDifference = difference;
+                        closestParty = party;
+                    }
+                }
+            } else {
+                for(let party of parties){
+                    const difference = party_opinion - party.values[axis.order];
+                    if(difference < minDifference && difference <= 0 && party.vote != this.vote){
+                        minDifference = difference;
+                        closestParty = party;
+                    }
+                }
+            }
+        }
+        if(closestParty == null){
+            return new Party("Niezrzeszeni", 0, [5, 6, 5, 6])
+        }
+        return closestParty;
+    }
 
     getExtremeCount() {
         return this.count * 0.05;
@@ -62,14 +124,40 @@ class Party {
         this.vote = vote;
     }
 
-    calculateMadPerAxis(legislation: Legislation, axis: Axis) {
-        let party_opinion = this.values[axis.order];
-        let legislation_value = legislation.values[axis.order];
+    calculateMad(legislation: Legislation, axis: Axis): number {
+        let mad: number = 0;
+
+        if(this.vote == Vote.HOLD){
+            return mad;
+        }
 
         
+        if(legislation.values[axis.order] <= 5){
 
+            let tolerance_barrier: number = legislation.values[axis.order] + 1;
+            if(this.vote == Vote.FOR){ //LEWICOWA ZA
+                for(let i = 9; i > tolerance_barrier - 1; i--){
+                    mad += this.count_per_opinion[axis.order][i];
+                }
+            } else { // LEWICOWA PRZECIW ???
+                for(let i = 0; i <= tolerance_barrier; i++){
+                    mad += this.count_per_opinion[axis.order][i];
+                }
+            }
+        } else{
+            let tolerance_barrier: number = legislation.values[axis.order] - 1;
+            if(this.vote == Vote.FOR){ //PRAWICOWA ZA
+                for(let i = 0; i < tolerance_barrier; i++){
+                    mad += this.count_per_opinion[axis.order][i];
+                }
+            } else { //PRAWICOWA PRZECIW
+                for(let i = 9; i >= tolerance_barrier - 1; i--){
+                    mad += this.count_per_opinion[axis.order][i];
+                }
+            }
+        }
+        return Math.ceil(mad);
     }
-
 }
 
 class Legislation {
@@ -86,32 +174,94 @@ class Legislation {
 }
 
 enum Vote {
-    For,
-    Against,
-    Hold
+    FOR = "FOR",
+    AGAINST = "AGAINST",
+    HOLD = "HOLD"
 }
 
 
 let numberOfAxes: number = 0;
 
-function calculateChanges(parties: Party[], legislation: Legislation){
-    for(let party of parties){
-       // party.calculateAngryCount();
-    }
 
+function calculateChanges(parties: Party[], axes: Axis[], legislation: Legislation){
+    for(let party of parties){
+        if(party.vote != Vote.HOLD){
+            for(let axis of axes){
+
+                let mad: number = party.calculateMad(legislation, axis);
+                let furious: number = Math.ceil(mad/5);
+                console.log(party.name + " " + axis.name + "Mad: " + mad);
+                let closestParty = party.findClosestParty(parties, axis, legislation, party.vote);
+                console.log(party.name + " " + axis.name + "ClosestParty: " + closestParty.name)
+                party.count -= furious;
+                console.log(party.name + " " + axis.name + "Party.count - mad: " + party.count)
+                if(closestParty == null){
+                    niezrzeszeni += furious;
+                } else {
+                    closestParty.count += furious;
+                    console.log(party.name + " " + axis.name + "ClosestParty.count + mad: " + closestParty.count)
+                }
+                
+            }
+        }
+    }
+    recalculateCountPerOpinion(parties);
+}
+
+function recalculateCountPerOpinion(parties: Party[]){
+    for(let party of parties){
+        for(let i = 0; i < 4; i++){
+            for(let j = 0; j < 10; j++){
+                if(party.values[i] == j - 1 || party.values[i] == j + 3){
+                    party.count_per_opinion[i][j] = party.getExtremeCount();
+                } else if(party.values[i] == j || party.values[i] == j + 2){
+                    party.count_per_opinion[i][j] = party.getLeaningCount();
+                } else if(party.values[i] == j + 1){
+                    party.count_per_opinion[i][j] = party.getBasisCount();
+                } else {
+                    party.count_per_opinion[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+function getFor(parties: Party[]){
+    let count: number = 0;
+    for(let party of parties){
+        if(party.vote == Vote.FOR) count += party.count;
+    }
+    return count;
+}
+
+function getAgainst(parties: Party[]){
+    let count: number = 0;
+    for(let party of parties){
+        if(party.vote == Vote.AGAINST) count += party.count;
+    }
+    return count;
+}
+
+function getHold(parties: Party[]){
+    let count: number = 0;
+    for(let party of parties){
+        if(party.vote == Vote.HOLD) count += party.count;
+    }
+    return count;
 }
 
 main();
+let niezrzeszeni: number = 0;
+
 function main(){
 
     let parties: Party[] = [];
-    let niezrzeszeni: number = 0;
 
     let pis = new Party("Prawo i Sprawiedliwość", 115, [4, 3, 7, 7]);
     parties.push(pis);
     let ko = new Party("Koalicja Obywatelska", 115, [6, 5, 4, 4]);
     parties.push(ko);
-    let lewica = new Party("Nowa Lewica", 115, [3,3,3,3]);
+    let lewica = new Party("Nowa Lewica", 115, [8,3,3,3]);
     parties.push(lewica);
     let konfa = new Party("Konfederacja", 115, [8, 7, 8, 8])
     parties.push(konfa);
@@ -128,16 +278,18 @@ function main(){
     let eu = new Axis("Stosunek do UE");
     axes.push(eu);
 
-    console.log(parties, axes);
 
-    let invitro = new Legislation("Finansowanie in Vitro", fiscal_policy, [-1, 4, 4, -1]);
+    let legislation = new Legislation("Ustawa", economic_policy, [7]);
 
-    pis.setVote(Vote.Against);
-    konfa.setVote(Vote.Hold);
-    lewica.setVote(Vote.For);
-    ko.setVote(Vote.For);
+    pis.voteAtRandom();
+    ko.voteAtRandom();
+    lewica.voteAtRandom();
+    ko.voteAtRandom();
 
-    calculateChanges(parties, invitro);
-    
+    console.log(parties);
+    console.log("Niezrzeszeni: " + niezrzeszeni)
+    calculateChanges(parties, axes, legislation);
+    console.log(parties);
+    console.log("Niezrzeszeni: " + niezrzeszeni)
 
 }
